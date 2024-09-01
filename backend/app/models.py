c81b32ab84e31ba5e269f12c932d0372c04b1c88
jpantonow <jpantonow@gmail.com>
2024-09-01 20:35:55 -0300

    helping on lucas changes

--- backend/app/models.py
+++ backend/app/models.py
@@ -43,8 +43,6 @@ class UpdatePassword(SQLModel):
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
-
 
 # Properties to return via API, id is always required
 class UserPublic(UserBase):
@@ -76,11 +74,6 @@ class ItemUpdate(ItemBase):
 class Item(ItemBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     title: str = Field(max_length=255)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="items")
-
 
 # Properties to return via API, id is always required
 class ItemPublic(ItemBase):

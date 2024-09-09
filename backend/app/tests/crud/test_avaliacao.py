b0d129ce9accbfc8133c8fffe1f27246b8d83661
jpantonow <jpantonow@gmail.com>
2024-09-09 17:51:11 -0300

    cleaning tests

--- backend/app/tests/crud/test_avaliacao.py
+++ backend/app/tests/crud/test_avaliacao.py
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-# from fastapi.encoders import jsonable_encoder
-# from sqlmodel import Session,select
-# import uuid
-# from app import crud
-# from app.core.security import verify_password
-# from app.models import Avaliacao,AvaliacaoBase,AvaliacaoCreate,AvaliacaoPublic,AvaliacoesPublic,Shape
-# from app.tests.utils.utils import random_email, random_lower_string
-# import secrets
-# import string
-# from .nome_gen import choose_name
-# import random
-# from .utils import especialidades, random_datetime,start_date,end_date
-# # cpf and telefone
-# N=8
-
-# def test_create_avaliacao(db: Session) -> None:
-#     for i in range(20):
-        
-#         id = random.randint(0,10000000)
-#         nome_foto=f"foto_{id}"
-        
-#         statement = select(Shape).where(Shape.id == id)
-#         shapes = db.exec(statement).first()
-#         # Caminho para a foto existente
-#         existing_image_file = ".shapes/caua.png"
-#         if shapes:
-#             continue
-        
-#         with open(existing_image_file, "rb") as img_file:
-#             file_contents = img_file.read()
-
-#         shape = Shape(
-#             id=id,
-#             nome_foto=nome_foto,
-#             foto=file_contents
-#         )
-        
-#         db.add(shape)
-#         db.commit()
-#         db.refresh(shape)
-        
-#         data_avaliacao = random_datetime(start_date, end_date)
-#         peso         = round(random.uniform(40.0, 170.0),2)
-#         altura       = round(random.uniform(1.3,2.2),2)
-#         perc_gordura = round(random.uniform(4.0,30.0),2)
-        
-#         avaliacao = AvaliacaoCreate(
-#             id=id,
-#             data_avaliacao = data_avaliacao,
-#             peso = peso,
-#             altura = altura,
-#             perc_gordura = perc_gordura,
-#             shape_id = id
-#         )
-        
-#         existing_avaliacao = crud.get_avaliacoes(session=db,id=id)
-#         if existing_avaliacao:
-#             continue
-        
-#         aval = crud.create_avaliacao(session=db,avaliacao_create=avaliacao)
-#         assert aval.id == id
-=======
 from fastapi.encoders import jsonable_encoder
 from sqlmodel import Session,select
 import uuid
@@ -117,4 +54,3 @@ def test_create_avaliacao(db: Session) -> None:
         
         aval = crud.create_avaliacao(session=db,avaliacao_create=avaliacao)
         assert aval.id == id
->>>>>>> merge-jp-lucas-teusdev-thfer
--- backend/app/tests/crud/test_user.py
+++ backend/app/tests/crud/test_user.py
@@ -5,13 +5,11 @@
 # from app.core.security import verify_password
 # from app.models import User, UserCreate, UserUpdate
 # from app.tests.utils.utils import random_email, random_lower_string
-# from app.cpf import generate_cpf
 
 # def test_create_user(db: Session) -> None:
 #     email = random_email()
 #     password = random_lower_string()
-#     cpf = generate_cpf()
-#     user_in = UserCreate(email=email, password=password,cpf=cpf)
+#     user_in = UserCreate(email=email, password=password)
 #     user = crud.create_user(session=db, user_create=user_in)
 #     assert user.email == email
 #     assert hasattr(user, "hashed_password")
@@ -20,8 +18,7 @@
 # def test_authenticate_user(db: Session) -> None:
 #     email = random_email()
 #     password = random_lower_string()
-#     cpf = generate_cpf()
-#     user_in = UserCreate(email=email, password=password,cpf=cpf)
+#     user_in = UserCreate(email=email, password=password)
 #     user = crud.create_user(session=db, user_create=user_in)
 #     authenticated_user = crud.authenticate(session=db, email=email, password=password)
 #     assert authenticated_user
@@ -70,8 +67,7 @@
 # def test_get_user(db: Session) -> None:
 #     password = random_lower_string()
 #     username = random_email()
-#     cpf = generate_cpf()
-#     user_in = UserCreate(email=username, password=password,cpf=cpf,is_superuser=True)
+#     user_in = UserCreate(email=username, password=password,is_superuser=True)
 #     user = crud.create_user(session=db, user_create=user_in)
 #     user_2 = db.get(User, user.id)
 #     assert user_2
@@ -82,8 +78,7 @@
 # def test_update_user(db: Session) -> None:
 #     password = random_lower_string()
 #     email = random_email()
-#     cpf = generate_cpf()
-#     user_in = UserCreate(email=email, password=password,cpf=cpf,is_superuser=True)
+#     user_in = UserCreate(email=email, password=password,is_superuser=True)
 #     user = crud.create_user(session=db, user_create=user_in)
 #     new_password = random_lower_string()
 #     user_in_update = UserUpdate(password=new_password, is_superuser=True)
